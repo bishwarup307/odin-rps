4cb2623444b93a5ea7fea696c830bd270ae183ea
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-02-01 22:26:01 +0530

    Add the game logic

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
+const playerChoice = document.querySelector("#player-choice");
+const resultEl = document.querySelector("#result");
+
 function getComputerChoice() {
   const randNum = Math.floor(Math.random() * 3) + 1;
   switch (randNum) {
     case 1:
-      return "Rock";
+      return "rock";
 
     case 2:
-      return "Paper";
+      return "paper";
 
     case 3:
-      return "Scissor";
+      return "scissor";
 
     default:
       return "Computer gone mad!";
   }
 }
 
-console.log(`Computer picked ${getComputerChoice()}`);
+function capitalize(string) {
+  titleString = string.at(0).toUpperCase() + string.substr(1).toLowerCase();
+  return titleString;
+}
+
+function playRound(playerChoice, computerChoice) {
+  let result;
+
+  result = playerChoice === computerChoice ? "tie" : null;
+
+  playerWin1 = playerChoice === "paper" && computerChoice === "rock";
+  playerWin2 = playerChoice === "scissor" && computerChoice === "paper";
+  playerWin3 = playerChoice === "rock" && computerChoice === "scissor";
+
+  result = result || (playerWin1 || playerWin2 || playerWin2 ? "win" : "lose");
+
+  finalResult = `You picked ${capitalize(
+    playerChoice
+  )}, computer picked ${capitalize(computerChoice)}. `;
+
+  finalResult =
+    result === "tie"
+      ? finalResult + `This was a tie`
+      : finalResult + `You ${result} this round.`;
+
+  console.log(finalResult);
+  return finalResult;
+}
+
+playerChoice.addEventListener("change", () => {
+  let result = playRound(playerChoice.value, getComputerChoice());
+  resultEl.textContent = result;
+});
